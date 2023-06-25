360c7cf44ce03d8eaca87d722c1ffe6545d5f441
bigmosi <kinyeramo@gmail.com>
2023-06-25 03:59:16 +0300

    apis

--- routes/class.js
+++ routes/class.js
@@ -33,43 +33,43 @@ router.post('/', async (req, res) => {
     }
   });
 
-  router.put('/:id', async (res, req) => {
+  router.put('/:id', async (req, res) => {
     try {
-        const classId = req.params.id;
-        const { name, teacher, schedule, subject, studentIds } = req.body;
-
-        const updatedClass = await Class.findOneAndUpdate(
-            { _id: classId },
-            { name, teacher, schedule, subject, studentIds },
-            { new: true }
-
-        );
-
-        if (!updatedClass) {
-            return res.status(404).json({ message: 'Class not found' });
-        }
-
-        res.status(200).json(updatedClass);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server Error' });
-    }
-  });
-
-  router.delete('/:id', async (req, res) => {
-    try {
-      const classId = req.para.id;
-
-      const deleteclass = await Class.findByIdAndDelete(classId);
-
-      if (!deleteclass) {
+      const classId = req.params.id;
+      const { name, teacher, schedule, subject, studentIds } = req.body;
+  
+      const updatedClass = await Class.findOneAndUpdate(
+        { _id: classId },
+        { name, teacher, schedule, subject, studentIds },
+        { new: true }
+      );
+  
+      if (!updatedClass) {
         return res.status(404).json({ message: 'Class not found' });
       }
-
+  
+      res.status(200).json(updatedClass);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });  
+  
+  
+  router.delete('/:id', async (req, res) => {
+    try {
+      const classId = req.params.id;
+  
+      const deleteClass = await Class.findByIdAndDelete(classId);
+  
+      if (!deleteClass) {
+        return res.status(404).json({ message: 'Class not found' });
+      }
+  
       res.json({ message: 'Class deleted successfully' });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server Error' });
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
     }
   });
   
