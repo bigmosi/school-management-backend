20f2253a8df09a1564e0740cdfd8afebb6217ed2
bigmosi <kinyeramo@gmail.com>
2023-06-25 10:59:28 +0300

    created the teacher modal

---
+++ models/Teacher.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const teacherSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    subjects: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+           ref: 'Subject'
+      }
+    ]
+});
+
+const Teacher = mongoose.model('Teacher', teacherSchema);
+
+module.exports = Teacher;
+
