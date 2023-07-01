f8e664e6c9aa97dd4b25cd9082e0fa21963e2bf9
bigmosi <kinyeramo@gmail.com>
2023-07-01 18:56:47 +0300

    added functionality for the upload file

--- routes/lessonPlan.js
+++ routes/lessonPlan.js
@@ -1,25 +1,44 @@
 const express = require('express');
 const LessonPlan = require('../models/LessonPlan');
+const multer = require('multer');
 const router = express.Router();
 
-router.post('/', upload.single('file'), (req, res) => {
- // Handle the file upload and other lesson plan data
-
- const lessonPlan = {
-    title: req.body.title,
-    description: req.body.description,
-    file: req.file,
-    teacher: req.body.teacher,
- }
-
- LessonPlan.create(lessonPlan)
- .then(savedLessonPlan => {
-    res.json(savedLessonPlan);
- }).catch(error => {
-    res.status(500).json({ error: 'An error occured while creating the lesson plan.' });
- });
-});
-
+const upload = multer({
+    storage: multer.diskStorage({
+      destination: 'uploads/',
+      filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, 'lessonplan-' + uniqueSuffix + '.pdf');
+      },
+    }),
+    fileFilter: (req, file, cb) => {
+      if (file.mimetype === 'application/pdf') {
+        cb(null, true);
+      } else {
+        cb(new Error('Only PDF files are allowed.'));
+      }
+    },
+  });
+  
+  router.post('/', upload.single('file'), (req, res) => {
+    // Handle the file upload and other lesson plan data
+  
+    const lessonPlan = {
+      title: req.body.title,
+      description: req.body.description,
+      file: req.file.path,
+      teacher: req.body.teacher,
+    };
+  
+    LessonPlan.create(lessonPlan)
+      .then(savedLessonPlan => {
+        res.json(savedLessonPlan);
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'An error occurred while creating the lesson plan.' });
+      });
+  });
+  
 router.get('/:teacherId', (req, res) => {
     const teacherId = req.params.teacherId;
 
@@ -31,3 +50,5 @@ router.get('/:teacherId', (req, res) => {
         res.status(500).json({ error: 'An error occured while trieving the lessons plan.'});
     })
 });
+
+module.exports = router;
