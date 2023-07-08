af579f3cdf7605ff75879f787168416f720650ef
kidde60 <kgeorgewilliam60@gmail.com>
2023-07-09 00:24:10 +0300

    create user model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Enter your name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Enter valid email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Enter your password']
+    },          
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
