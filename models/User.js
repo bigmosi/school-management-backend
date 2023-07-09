e4857c27842f0e20badfc5f74817c6c8638186be
kidde60 <kgeorgewilliam60@gmail.com>
2023-07-09 18:13:14 +0300

    Add register user

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -17,5 +18,18 @@ const userSchema = mongoose.Schema({
 }, {
     timestamps: true
 })
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 module.exports = mongoose.model('User', userSchema)
