3081c91cd5d9d5ea0b31bdfdf14be7c469442215
bigmosi <kinyeramo@gmail.com>
2023-07-27 18:40:34 +0300

    added user registration functionalities

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Check if the username already exists
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(409).json({ message: "Username already exists" });
+        }
+
+        // Hash the password before saving it to the database 
+        const hasedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user
+        const newUser = new User({
+            username,
+            password: hasedPassword,
+        });
+
+        await newUser.save();
+
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
