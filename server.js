ce66bfc5b042883ea334ae95c02e03cf99ec21fd
kidde60 <kgeorgewilliam60@gmail.com>
2023-09-28 08:57:05 +0300

    Add auth

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const debug = require('debug')('server');
-const chalk = require('chalk');
-const cors = require('cors');
-const studentRoutes = require('./routes/studentRoutes');
-const admission = require('./routes/admission');
-const attendance = require('./routes/attendance');
-const report = require('./routes/report');
-const classes = require('./routes/class');
-const teacher = require('./routes/teacher');
-const schedule = require('./routes/schedule');
-const subject = require('./routes/subject');
-const announcement = require('./routes/announcement');
-const lesson = require('./routes/lessonPlan');
-const examSchedule = require('./routes/examSchedule.js');
-const question = require('./routes/question');
-const exam = require('./routes/Exam');
-const path = require('path');
-const admin = require('./routes/users');
+const express = require("express");
+const mongoose = require("mongoose");
+const debug = require("debug")("server");
+const chalk = require("chalk");
+const cors = require("cors");
+const studentRoutes = require("./routes/studentRoutes");
+const admission = require("./routes/admission");
+const attendance = require("./routes/attendance");
+const report = require("./routes/report");
+const classes = require("./routes/class");
+const teacher = require("./routes/teacher");
+const schedule = require("./routes/schedule");
+const subject = require("./routes/subject");
+const announcement = require("./routes/announcement");
+const lesson = require("./routes/lessonPlan");
+const examSchedule = require("./routes/examSchedule.js");
+const question = require("./routes/question");
+const exam = require("./routes/Exam");
+const path = require("path");
+const admin = require("./routes/users");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -25,29 +25,32 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Register the  routes
-app.use('/api/students', studentRoutes);
-app.use('/api/admissions', admission);
-app.use('/api/attendance', attendance);
-app.use('/api/reports', report);
-app.use('/api/classes', classes);
-app.use('/api/teachers', teacher);
-app.use('/api/schedules', schedule);
-app.use('/api/subjects', subject);
-app.use('/api/announcements', announcement);
-app.use('/api/lesson-plans', lesson);
-app.use('/api/exam-schedules', examSchedule);
-app.use('/api/questions', question);
-app.use('/api/exams', exam);
-app.use('/api/register/admin', admin);
+app.use("/api/students", studentRoutes);
+app.use("/api/admissions", admission);
+app.use("/api/attendance", attendance);
+app.use("/api/reports", report);
+app.use("/api/classes", classes);
+app.use("/api/teachers", teacher);
+app.use("/api/schedules", schedule);
+app.use("/api/subjects", subject);
+app.use("/api/announcements", announcement);
+app.use("/api/lesson-plans", lesson);
+app.use("/api/exam-schedules", examSchedule);
+app.use("/api/questions", question);
+app.use("/api/exams", exam);
+app.use("/api/register/admin", admin);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    "mongodb+srv://mracheal180:William60@schoolcluster.zmrdnsj.mongodb.net/test"
+  )
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
     app.listen(PORT, () => {
+      console.log(PORT);
       debug(`Server is running on port ${chalk.green(PORT)}`);
     });
   })
