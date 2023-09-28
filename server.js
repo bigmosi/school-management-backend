46a756aa64226426d1c68a6f81d5a332066ccc8b
kidde60 <kgeorgewilliam60@gmail.com>
2023-09-28 09:37:42 +0300

    URI

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.use("/api/questions", question);
 app.use("/api/exams", exam);
 app.use("/api/register/admin", admin);
 
-const URI =
-  "mongodb+srv://mracheal180:William60@schoolcluster.zmrdnsj.mongodb.net/test";
+const URI = process.env.MONGO_URI;
+
 mongoose
   .connect(URI)
   .then(() => {
